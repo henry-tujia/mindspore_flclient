27768ccfdd08702f770b171b077fbd9da457607b
henry-tujia <1319670017@qq.com>
2021-12-20 21:19:17 +0800

    loss name

--- mindspore/lite/include/train/train_cfg.h
+++ mindspore/lite/include/train/train_cfg.h
@@ -16,6 +16,7 @@
 #ifndef MINDSPORE_LITE_INCLUDE_TRAIN_TRAIN_CFG_H_
 #define MINDSPORE_LITE_INCLUDE_TRAIN_TRAIN_CFG_H_
 #include <string>
+#include <vector>
 
 namespace mindspore {
 namespace lite {
@@ -28,34 +29,32 @@ class MixPrecisionCfg {
     this->loss_scale_ = 128.0f;
     this->keep_batchnorm_fp32_ = true;
     this->num_of_not_nan_iter_th_ = 1000;
-    this->is_raw_mix_precision_ = false;
   }
   MixPrecisionCfg(const MixPrecisionCfg &rhs) {
     this->dynamic_loss_scale_ = rhs.dynamic_loss_scale_;
     this->loss_scale_ = rhs.loss_scale_;
     this->keep_batchnorm_fp32_ = rhs.keep_batchnorm_fp32_;
     this->num_of_not_nan_iter_th_ = rhs.num_of_not_nan_iter_th_;
-    this->is_raw_mix_precision_ = rhs.is_raw_mix_precision_;
   }
   MixPrecisionCfg &operator=(MixPrecisionCfg const &rhs) {
     this->dynamic_loss_scale_ = rhs.dynamic_loss_scale_;
     this->loss_scale_ = rhs.loss_scale_;
     this->keep_batchnorm_fp32_ = rhs.keep_batchnorm_fp32_;
     this->num_of_not_nan_iter_th_ = rhs.num_of_not_nan_iter_th_;
-    this->is_raw_mix_precision_ = rhs.is_raw_mix_precision_;
     return *this;
   }
-  bool dynamic_loss_scale_ = false;   /**< Enable\disable dynamic loss scale during mix precision training */
-  float loss_scale_;                  /**< Initial loss scale factor  */
-  bool keep_batchnorm_fp32_ = true;   /**< Keep batch norm in FP32 while training */
-  uint32_t num_of_not_nan_iter_th_;   /**< a threshold for modifying loss scale when dynamic loss scale is enabled */
-  bool is_raw_mix_precision_ = false; /**< Is mix precision model export from mindspore  */
+  bool dynamic_loss_scale_ = false; /**< Enable\disable dynamic loss scale during mix precision training */
+  float loss_scale_;                /**< Initial loss scale factor  */
+  bool keep_batchnorm_fp32_ = true; /**< Keep batch norm in FP32 while training */
+  uint32_t num_of_not_nan_iter_th_; /**< a threshold for modifying loss scale when dynamic loss scale is enabled */
 };
 
 /// \brief TrainCfg defined for holding train configuration.
 class TrainCfg {
  public:
-  TrainCfg() { this->loss_name_ = "_loss_fn"; }
+  TrainCfg() { this->loss_name_.emplace_back("SigmoidCrossEntropyWithLogits");}
+	  //this->loss_name_.emplace_back("_loss_fn");}
+		//this->loss_name_.emplace_back("SigmoidCrossEntropyWithLogits"); }
   TrainCfg(const TrainCfg &rhs) {
     this->loss_name_ = rhs.loss_name_;
     this->mix_precision_cfg_ = rhs.mix_precision_cfg_;
@@ -67,9 +66,9 @@ class TrainCfg {
     this->accumulate_gradients_ = rhs.accumulate_gradients_;
     return *this;
   }
-  std::string loss_name_;             /**< Set part of the name that identify a loss kernel */
-  MixPrecisionCfg mix_precision_cfg_; /**< Mix precision configuration */
-  bool accumulate_gradients_ = false; /**< If true gardents are accmulated and can be read by GetGradients */
+  std::vector<std::string> loss_name_; /**< Set part of the name that identify a loss kernel */
+  MixPrecisionCfg mix_precision_cfg_;  /**< Mix precision configuration */
+  bool accumulate_gradients_ = false;  /**< If true gardents are accmulated and can be read by GetGradients */
 };
 
 }  // namespace lite
